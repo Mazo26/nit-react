39e9152a9ae07a092e9fbd41cc24586d58be5302
Mazo26 <mazo.creed26@gmail.com>
2026-05-24 17:52:10 +0200

    teorija + izmene na projektu

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import "./Header.css";
+
+function Header() {
+  return (
+    <nav className='navigationBar'>
+      <a href='' className='navLinks'>
+        Pocetna
+      </a>
+      <a href='' className='navLinks'>
+        O nama
+      </a>
+      <a href='' className='navLinks'>
+        Kontakt
+      </a>
+    </nav>
+  );
+}
+
+export default Header;
