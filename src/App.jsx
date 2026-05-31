2ba8b8a05145bd026c6446866a4a6a6fb0af86d8
Mazo26 <mazo.creed26@gmail.com>
2026-05-31 18:53:53 +0200

    props react

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,45 @@ import Card from "./components/Card/Card";
 import Header from "./components/Header/Header";
 
 function App() {
+  const kartice = [
+    {
+      src: "https://radovic-enterijer.com/wp-content/uploads/2025/03/Stolica_City-1-scaled.jpg",
+      heading: "Card heading 1",
+      description: "Description 1",
+      onClickBtn: () => {
+        console.log("Card heading 1");
+      },
+    },
+    {
+      src: "https://radovic-enterijer.com/wp-content/uploads/2025/03/Stolica_City-1-scaled.jpg",
+      heading: "Card heading 2",
+      description: "Description 2",
+      onClickBtn: () => {
+        console.log("Card heading 2");
+      },
+    },
+    {
+      src: "https://radovic-enterijer.com/wp-content/uploads/2025/03/Stolica_City-1-scaled.jpg",
+      heading: "Card heading 3",
+      description: "Description 3",
+      onClickBtn: () => {
+        console.log("Card heading 3");
+      },
+    },
+  ];
+
   return (
     <>
       <Header />
       <div className='card-wrapper'>
-        <Card />
-        <Card />
-        <Card />
+        {kartice.map((kartica) => (
+          <Card
+            imgSrc={kartica.src}
+            cardHeading={kartica.heading}
+            cardDesc={kartica.description}
+            btnClick={kartica.onClickBtn}
+          />
+        ))}
       </div>
     </>
   );
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,14 +1,16 @@
 import "./Card.css";
 
-function Card() {
-  const imageSource =
-    "https://radovic-enterijer.com/wp-content/uploads/2025/03/Stolica_City-1-scaled.jpg";
+function Card(props) {
+  const { imgSrc, cardHeading, cardDesc, btnClick } = props;
+  console.log(imgSrc, cardDesc, cardHeading);
   return (
     <div className='card-container'>
-      <img src={imageSource} alt='Card image here' className='card-img' />
-      <p className='card-heading'>Card heading</p>
-      <p className='card-description'>Card description</p>
-      <button className='card-button'>See more</button>
+      <img src={imgSrc} alt='Card image here' className='card-img' />
+      <p className='card-heading'>{cardHeading}</p>
+      <p className='card-description'>{cardDesc}</p>
+      <button className='card-button' onClick={btnClick}>
+        See more
+      </button>
     </div>
   );
 }
